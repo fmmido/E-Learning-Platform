7cb04189257997e96fc0b69ce8935f37ca8dc63a
s2yed <s2yedftouh@gmail.com>
2024-12-29 22:04:51 +0200

    fix static1

--- project/project/settings.py
+++ project/project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-o4gkync!zq49gc)bv@!1hlb0i_@!1p_9pyd+m94i-@y!-8b+93
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG determines if the project is in debug mode. Should be False in production
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS defines the allowed hostnames for the Django application
 ALLOWED_HOSTS = ["*"]
